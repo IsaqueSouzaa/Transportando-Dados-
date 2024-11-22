88b0491109f61cef3a426f52e483df1240a07dcd
Isaque Barbosa <isaquebarbosa463@gmail.com>
2024-11-21 23:31:47 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header> 
+      <p>Orion Transportes</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
